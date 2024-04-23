65f46a153b5e16a7dc62366aeef076e4af5aaf02
Taifun20 <ciubucvali3@gmail.com>
2024-04-23 20:05:47 +0300

    v0.1 - Base

--- Mostenirea/Mostenirea.cpp
+++ Mostenirea/Mostenirea.cpp
@@ -1,7 +1,17 @@
 
 #include <iostream>
+#include "Vehicle_.h"
+
+
+void FirstCar_()
+{
+	Car_ BMW_M3_GTR_;
+	BMW_M3_GTR_.Brand = "BMW";
+	BMW_M3_GTR_.StartEng();
+	BMW_M3_GTR_.Honk_();
+}
 
 int main()
 {
-
+	FirstCar_();
 }
