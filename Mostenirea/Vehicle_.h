4476f40e7b356ac023351ed1163495c9be30e699
Taifun20 <ciubucvali3@gmail.com>
2024-04-23 20:14:54 +0300

    Added Constructor / Destructor to class Vehicle_ and Car_

--- Mostenirea/Vehicle_.h
+++ Mostenirea/Vehicle_.h
@@ -5,6 +5,16 @@
 class Vehicle_
 {
 	public:
+
+		Vehicle_()
+		{
+			std::cout << "	Class Vehicle_ Created" << std::endl;
+		}
+		~Vehicle_()
+		{
+			std::cout << "	Class Vehicle_ Destroyed" << std::endl;
+		}
+
 		std::string Brand;
 		void StartEng()
 		{
@@ -16,6 +26,16 @@ class Vehicle_
 class Car_ : public Vehicle_
 {
 	public:
+
+		Car_()
+		{
+			std::cout << "	BP Car_ Created" << std::endl;
+		}
+		~Car_()
+		{
+			std::cout << "	BP Car_ Destroyed" << std::endl;
+		}
+
 		void Honk_()
 		{
 			std::cout << "	Beep Beep!" << std::endl;
